9a9bc08693b54b1cb0c41b4e77adb960f92d6722
b73972324-source <b73972324@gmail.com>
2026-02-06 00:07:34 +0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,25 @@
+package com.example.demo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private int age;
+}
